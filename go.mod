a4312dba2c91f93209230b0add98b030c013083c
Perny <pernydev@gmail.com>
2024-01-06 20:32:42 +0200

    as simple as that!

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/pernydev/gomix
+
+go 1.21.5
+
+require (
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/the-jonsey/pulseaudio v0.0.1 // indirect
+	gitlab.com/gomidi/midi/v2 v2.0.30 // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/the-jonsey/pulseaudio"
+	"gitlab.com/gomidi/midi/v2"
+	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv" // autoregisters driver
+)
+
+func main() {
+	defer midi.CloseDriver()
+
+	in, err := midi.FindInPort("nanoKONTROL2")
+	if err != nil {
+		fmt.Println("can't find midi")
+		return
+	}
+
+	if err != nil {
+		fmt.Println("can't find midi")
+		return
+	}
+
+	// connect to the system's pulseaudio server
+	client, err := pulseaudio.NewClient()
+
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+	bindings := map[uint8]string{
+		0: "dc-source",
+		1: "dc-output",
+		2: "fun-output",
+		3: "fun-tocord",
+	}
+
+	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
+		var bt []byte
+		var ch, key, vel uint8
+		switch {
+		case msg.GetSysEx(&bt):
+			fmt.Printf("got sysex: % X\n", bt)
+		case msg.GetControlChange(&ch, &key, &vel):
+			fmt.Printf("got control change: %d %d %d\n", ch, key, vel)
+			if bindings[key] != "" {
+				client.SetSinkVolume(bindings[key], float32(vel)/127)
+				fmt.Printf("Setting %s to %f\n", bindings[key], float32(vel)/127)
+			}
+		}
+
+	}, midi.UseSysEx())
+
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+	fmt.Scanln()
+
+	stop()
+}
