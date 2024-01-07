f3357a6cb053918fb28de3bb4c3560c44aaf40f9
Perny <pernydev@gmail.com>
2024-01-07 14:40:35 +0200

    Auto stash before revert of "whoah thats a lot of features"

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # gomix
+
 A simple MIDI to DBus mixer built Golang. It's hardcoded for my pipewire setup.
 
 ## Installation
-Clone the repository and copy the binary to /usr/local/bin. Then, copy the systemd service file to /etc/systemd/system and enable it.
+
+Clone the repository and copy the binary to /usr/local/bin. Then, copy the systemd service file to $HOME/.local/share/systemd/user and enable it.
 
 ```bash
 git clone https://github.com/pernydev/gomix.git
 sudo cp gomix/gomix /usr/local/bin
-sudo cp gomix/gomix.service /etc/systemd/system
-sudo systemctl enable gomix.service
-sudo systemctl start gomix.service
+sudo chmod +x /usr/local/bin/gomix
+cp gomix/gomix.service $HOME/.local/share/systemd/user
+systemctl --user enable gomix.service
+systemctl --user start gomix.service
 ```
--- main.go
+++ main.go
@@ -1,41 +1,122 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
 	"github.com/the-jonsey/pulseaudio"
 	"gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv" // autoregisters driver
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+const (
+	padding  = 2
+	maxWidth = 80
+)
+
+type channel struct {
+	Sink string
+	Vol  float32
+	Prog progress.Model
+	Name string
+}
+
+var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
+
+type tickMsg time.Time
+
+var channels = []channel{
+	0: {Sink: "dc_source", Vol: 0, Name: "Mic          "},
+	1: {Sink: "dc_output", Vol: 0, Name: "DC Output    "},
+	2: {Sink: "fun_to_headphones", Vol: 0, Name: "Headphones   "},
+	3: {Sink: "fun_to_dc", Vol: 0, Name: "Desktop to DC"},
+	4: {Sink: "fun_to_speakers", Vol: 0, Name: "Speakers     "},
+}
+
+type model struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return tickCmd()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+
+	case tea.WindowSizeMsg:
+		/*
+			m.progress.Width = msg.Width - padding*2 - 4
+			if m.progress.Width > maxWidth {
+				m.progress.Width = maxWidth
+			}
+		*/
+
+		for ch, channel := range channels {
+			channel.Prog.Width = msg.Width - padding*2 - 4
+			if channel.Prog.Width > maxWidth {
+				channel.Prog.Width = maxWidth
+			}
+			channels[ch] = channel
+		}
+
+		return m, nil
+	case tickMsg:
+		return m, tickCmd()
+	default:
+		return m, nil
+	}
+}
+
+func (m model) View() string {
+	pad := strings.Repeat(" ", padding)
+	var b strings.Builder
+	for _, channel := range channels {
+		b.WriteString(pad)
+		b.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render(channel.Name))
+		b.WriteString(pad)
+		b.WriteString(channel.Prog.ViewAs(float64(channel.Vol)))
+		b.WriteString("\n\n")
+	}
+	b.WriteString(helpStyle("Press any key to quit"))
+	return b.String()
+
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Millisecond*50, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
+
 func main() {
+	logger := log.New(os.Stdout)
+
+	logger.Info("Starting up...")
 	defer midi.CloseDriver()
 
 	in, err := midi.FindInPort("nanoKONTROL2")
 	if err != nil {
-		fmt.Println("can't find midi")
-		return
+		logger.Fatal("Can't find midi. Don't mean to be rude but.. Do you have a nanoKONTROL2? Is it plugged in? This program only works with a nanoKONTROL2", "error", err)
 	}
-
-	if err != nil {
-		fmt.Println("can't find midi")
-		return
-	}
-
 	// connect to the system's pulseaudio server
 	client, err := pulseaudio.NewClient()
 
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
-		return
+		logger.Fatal("Can't connect to pulseaudio. Is the program running on the wrong user?", "error", err)
 	}
 
-	bindings := map[uint8]string{
-		0: "dc-source",
-		1: "dc-output",
-		2: "fun-toheadphones",
-		3: "fun-tocord",
-		4: "fun-tospeakers",
+	for ch, channel := range channels {
+		channel.Prog = progress.NewModel(progress.WithDefaultGradient())
+		channel.Prog.ShowPercentage = true
+		channels[ch] = channel
 	}
 
 	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
@@ -43,23 +124,22 @@ func main() {
 		var ch, key, vel uint8
 		switch {
 		case msg.GetSysEx(&bt):
-			fmt.Printf("got sysex: % X\n", bt)
+			logger.Debug("Got SysEx", bt)
 		case msg.GetControlChange(&ch, &key, &vel):
-			fmt.Printf("got control change: %d %d %d\n", ch, key, vel)
-			if bindings[key] != "" {
-				client.SetSinkVolume(bindings[key], float32(vel)/127)
-				fmt.Printf("Setting %s to %f\n", bindings[key], float32(vel)/127)
+			if int(key) < len(channels) {
+				channels[key].Vol = float32(vel) / 127
+				client.SetSinkVolume(channels[key].Sink, channels[key].Vol)
 			}
 		}
-
 	}, midi.UseSysEx())
 
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
-		return
+		logger.Fatal("Can't listen to midi", "error", err)
 	}
 
-	fmt.Scanln()
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		logger.Fatal("Can't start program", "error", err)
+	}
 
 	stop()
 }
